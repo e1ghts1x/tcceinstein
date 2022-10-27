4a7345c36105531bfc58a3d1575ed908d23e8ef8
e1ghts1x <ekgamesbr@outlook.com>
2022-10-27 19:34:40 -0300

    27/10/2022 19:34 Home.css and routes.js fixing

--- sistema-de-analise-corporativo-academico/client/src/Routes/routes.js
+++ sistema-de-analise-corporativo-academico/client/src/Routes/routes.js
@@ -1,22 +1,19 @@
 import React from "react";
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Link, } from "react-router-dom";
 
 import Dashboard from "../Components/Dashboard/Dashboard";
 import Home from "../Components/Home/Home";
 import LoginAdmin from "../Components/LoginAdmin/LoginAdmin";
 import Quest from "../Components/Quest/Quest";
 
-const usuarioLogado = localStorage.getItem("@user");
-const adminLogado = localStorage.getItem("@admin")
-
 const Rotas = () => {
     return(
         <BrowserRouter>
             <Routes>
-                {!usuarioLogado && <Route path='/' element={<Home logado={usuarioLogado}/>}/>}
-                {usuarioLogado && <Route path='/' element={<Quest logado={usuarioLogado}/>}/>}
-                {adminLogado && <Route path='/admin' exact element={<Dashboard/>}/>}
-                {!adminLogado && <Route path='/admin' element={<LoginAdmin adminLogado={adminLogado}/>}/>}
+                <Route path="/" element={<Home/>}/>
+                <Route path="/quest" element={<Quest/>}/>
+                <Route path="/admin" element={<LoginAdmin/>}/>
+                <Route path="/dashboard" element={<Dashboard/>}/>
             </Routes>
         </BrowserRouter>
     )
